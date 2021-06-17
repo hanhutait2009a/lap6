d763e0532ad15bd2b928e66c94fe24f6ee221934
Your Name <you@example.com>
2021-06-17 07:26:12 +0700

    hanhutai-lab6

---
+++ ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using lab6;
+
+namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thermostat thermostat = new Thermostat();
+            Heater heater = new Heater(60);
+            thermostat.OnTemperatureChange +=
+                heater.OnTemperatureChanged;
+            Heater heaterCuong = new Heater(100);
+            thermostat.OnTemperatureChange +=
+                heaterCuong.OnTemperatureChanged;
+            Cooler cooler = new Cooler(80);
+            thermostat.OnTemperatureChange +=
+                cooler.OnTemperatureChanged;
+            string temperature;
+            Console.Write("Enter temperature: ");
+            temperature = Console.ReadLine();
+            thermostat.CurrentTemperature = int.Parse(temperature);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab6
+{
+    internal abstract class Program
+    {
+        public abstract void Increment(ref int X);
+
+        private static void Add2(ref int x)
+            {
+                x += 2;
+            }
+
+        private static void Add3(ref int x)
+            {
+                x += 3;
+            }
+            public static void Main()
+            {
+                Increment functionDelegate = Add2;
+                functionDelegate += Add3;
+                functionDelegate += Add2;
+                int x = 5;
+                functionDelegate(ref x);
+                Console.ReadLine();
+            }
+    }
+}
